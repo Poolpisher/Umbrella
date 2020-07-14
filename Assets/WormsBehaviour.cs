349ea6df075f4e3a1466868306ac854112f0c579
Rémy <remy.melinon.71@gmail.com>
2020-07-14 16:41:41 +0200

    Animation Vers
    Création d'un sprite et d'une animation pour l'IA du premier ennemi

---
+++ Assets/WormsBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormsBehaviour : MonoBehaviour
+{
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Move()
+    {
+    //infini:
+        myAnimator.SetBool("Limit", true);
+        //Marcher a gauche
+        myAnimator.SetBool("Limit", false);
+        //Marcher a droite
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
